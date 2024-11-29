731483e3109c1b80aaa027d9697e041c173582ef
Esraa Masri <esraa4masri@gmail.com>
2024-11-29 18:30:50 +0300

    first commit

---
+++ JS/Java.js
@@ -0,0 +1,31 @@
+
+function gotoPayament(){
+    window.location.href = "Paymanet.html";
+}
+function GotoUser(){
+    window.location.href = "Users.html";
+}
+function GotoEmployee(){
+    window.location.href = "Employee.html";
+}
+function OffersPage(){
+    window.location.href = "order.html";
+}
+function GotoChart(){
+    window.location.href = "charts.html";
+}
+function GotoNote(){
+    window.location.href = "Note.html";
+}
+function GotoReports(){
+    window.location.href = "Reports.html";
+}
+//لفتح وتسكير الsidebar
+const sidebarToggle = document.body.querySelector('#sidebarToggle');
+    if (sidebarToggle) {
+      sidebarToggle.addEventListener('click', event => {
+      event.preventDefault();
+      document.body.classList.toggle('sb-sidenav-toggled');
+      localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
+      });
+    }
